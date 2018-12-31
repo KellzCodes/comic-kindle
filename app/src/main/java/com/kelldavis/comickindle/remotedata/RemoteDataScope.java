c10fae9d7f5ed2b4b7cded65d95680a6b2e880b8
Kelli <keldavis5@yahoo.com>
2018-12-30 20:39:38 -0500

    create remote data scope

---
+++ app/src/main/java/com/kelldavis/comickindle/remotedata/RemoteDataScope.java
@@ -0,0 +1,12 @@
+package com.kelldavis.comickindle.remotedata;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RemoteDataScope {
+
+}
